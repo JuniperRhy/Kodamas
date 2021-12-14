f562ee180c4ce5658d4e38307e083cf351acf9f3
JuniperRhy <Gwyniver.Rhy@gmail.com>
2021-12-14 13:32:28 -0600

    add grid for scaling, improve sound, add mute button

--- kodamas/src/App.js
+++ kodamas/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import { Howl, Howler } from "howler";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+
+import MuteButton from "./MuteButton";
+
 import Kodama1 from "./kodamas/kodama1";
 import Kodama2 from "./kodamas/kodama2";
-
 import KodamaCouple from "./kodamas/kodamaCouple";
+
 import KodamaBG1 from "./kodamas/kodamaBG1";
 import KodamaBG2 from "./kodamas/kodamaBG2";
 import KodamaBG3 from "./kodamas/kodamaBG3";
 import KodamaBG4 from "./kodamas/kodamaBG4";
+
 import kodamaShake from "./media/kodamaShake.mp3";
 import kodamaShake2 from "./media/kodamaShake2.mp3";
 import kodamaShake3 from "./media/kodamaShake3.mp3";
@@ -14,12 +21,12 @@ import kodamaShake4 from "./media/kodamaShake4.mp3";
 import kodamaShake5 from "./media/kodamaShake5.mp3";
 import kodamaShake6 from "./media/kodamaShake6.mp3";
 import kodamaShake7 from "./media/kodamaShake7.mp3";
-import pianoMusic from "./media/pianoMusic.mp3";
+
 import calmForest from "./media/calmForest.mp3";
-import { Howl, Howler } from "howler";
-import { useState } from "react";
+import erhuMusic from "./media/erhu.mp3";
 
 function App() {
+  const [isPageMuted, setIsPageMuted] = useState(false);
   const [kodamaBG1IsClicked, setKodamaBG1IsClicked] = useState(false);
   const [kodamaBG2IsClicked, setKodamaBG2IsClicked] = useState(false);
   const [kodamaBG3IsClicked, setKodamaBG3IsClicked] = useState(false);
@@ -48,22 +55,21 @@ function App() {
     volume: 0.35,
   });
 
-  const backgroundSound = new Howl({
-    src: pianoMusic,
-    volume: 0.15,
-    loop: false,
-  });
-
   const backgroundAmbience = new Howl({
     src: calmForest,
     volume: 0.05,
     loop: true,
   });
 
-  Howler.volume(0.2);
+  Howler.volume(isPageMuted ? 0 : 0.2);
 
   // console.log(kodama1IsClicked);
-  console.log(kodama2IsClicked);
+  console.log(isPageMuted);
+
+  const [play, { stop }] = useSound(erhuMusic, {
+    volume: isPageMuted ? "0" : "0.08",
+    loop: true,
+  });
 
   return (
     <div className="App">
@@ -71,12 +77,21 @@ function App() {
         className="background"
         onMouseEnter={() => {
           backgroundAmbience.play();
+          play();
+        }}
+        onMouseLeave={() => {
+          backgroundAmbience.stop();
+          stop();
         }}
         onClick={() => {
           // backgroundSound.play();
         }}
       >
         <div className="contentWrapper">
+          <MuteButton
+            isPageMuted={isPageMuted}
+            setIsPageMuted={setIsPageMuted}
+          />
           <div className="displayText">
             <div className="displayText1">Welcome </div>
             <div className="displayText2">To </div>
@@ -94,19 +109,9 @@ function App() {
             setIsClicked={setKodamaBG2IsClicked}
             sound={sound}
           />
-          <KodamaBG3
-            isClicked={kodamaBG3IsClicked}
-            setIsClicked={setKodamaBG3IsClicked}
-            sound={sound}
-          />
-          <KodamaBG4
-            isClicked={kodamaBG4IsClicked}
-            setIsClicked={setKodamaBG4IsClicked}
-            sound={sound}
-          />
-          <Kodama1
-            isClicked={kodama1IsClicked}
-            setIsClicked={setKodama1IsClicked}
+          <Kodama2
+            isClicked={kodama2IsClicked}
+            setIsClicked={setKodama2IsClicked}
             sound={sound}
           />
 
@@ -118,13 +123,24 @@ function App() {
             sound={sound}
           />
 
-          <Kodama2
-            isClicked={kodama2IsClicked}
-            setIsClicked={setKodama2IsClicked}
+          <Kodama1
+            isClicked={kodama1IsClicked}
+            setIsClicked={setKodama1IsClicked}
+            sound={sound}
+          />
+          <KodamaBG3
+            isClicked={kodamaBG3IsClicked}
+            setIsClicked={setKodamaBG3IsClicked}
+            sound={sound}
+          />
+          <KodamaBG4
+            isClicked={kodamaBG4IsClicked}
+            setIsClicked={setKodamaBG4IsClicked}
             sound={sound}
           />
         </div>
       </div>
+      <div className="BackgroundBlur"></div>
     </div>
   );
 }
